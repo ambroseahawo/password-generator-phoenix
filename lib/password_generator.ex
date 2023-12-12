2f1a9c921e07e1df987d8d47e11883144ac52e02
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-12 12:38:18 +0300

    Validate Length

--- lib/password_generator.ex
+++ lib/password_generator.ex
@@ -40,4 +40,32 @@ defmodule PasswordGenerator do
     "abc3f"
 
   """
+
+  @spec generate(options :: map()) :: {:ok, bitstring()} | {:error, bitstring()}
+  def generate(options) do
+    length = Map.has_key?(options, "length")
+    validate_length(length, options)
+  end
+
+  defp validate_length(false, _options), do: {:error, "Please provide a length"}
+
+  defp validate_length(true, options) do
+    numbers = Enum.map(0..9, & Integer.to_string(&1))
+    length = options["length"]
+    length = String.contains?(length, numbers)
+    validate_length_is_integer(length, options)
+  end
+
+  defp validate_length_is_integer(false, _options) do
+    {:error, "Only integers allowed for length."}
+  end
+
+  defp validate_length_is_integer(true, options) do
+    length = options["length"] |> String.trim() |> String.to_integer()
+    options_without_length = Map.delete(options, "length")
+    options_values = Map.values((options_without_length))
+    value = options_values |> Enum.all?(fn x -> String.to_atom(x) |> is_boolean() end)
+    validate_options_values_are_boolean(value, length, options_without_length)
+  end
+
 end
